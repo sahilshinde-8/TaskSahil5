5b6e2e88ba75701aa00f95c58d2951e965bf1161
sahilshinde_8 <69679952+sahilshinde-8@users.noreply.github.com>
2023-07-21 20:37:39 +0530

    Update TicTacTest.java

--- TicTacTest.java
+++ TicTacTest.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 public class TicTacTest {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Rows ");
 		int rows = sc.nextInt();
+		System.out.println("Enter the Columns ");
 		int columns = sc.nextInt();
 	
 		if(rows==columns) {
@@ -12,14 +14,24 @@ public class TicTacTest {
 		String a[][] = new String [rows][columns];
 		for(int i=0; i < rows; i++) {
 			System.out.println("Player 1");
+			System.out.println("Please Select any one block ");
 			int l = sc.nextInt();
 			int p = sc.nextInt();
 			a[l][p] = "X";
 			System.out.println("Player 2");
+			System.out.println("Please Select any one block ");
 			int x = sc.nextInt();
 			int y = sc.nextInt();
 			a[x][y] = "O";
-	
+			game(i, a);
+		}
+		}
+		else {
+			System.out.println("Kindly enter same rows and columns ");
+		}
+	}
+		
+		private static void game(int i, String[][] a){
 	
             String line = null;
  
@@ -56,25 +68,26 @@ public class TicTacTest {
                 line = a[0][2] + a[1][1]+ a[2][0];
                 break;
             }
+            
             //For X winner
             if (line.equals("XXX")) {
-                //return "X";
-		    System.out.println("Player 1 Win");
+            	//return "X";
+            	System.out.println("X Wins");
             }
              
             // For O winner
             else if (line.equals("OOO")) {
-               // return "O";
-		     System.out.println("Player 2 Win");
+               //return "O";
+            	System.out.println("O Wins");
             }
-        }
-         
+            }
+        
 	
-	}
-	
-	else {
-		System.out.println("Kindly enter same rows and columns ");
-	}
-	}
+		}
 
-}
+		
+	
+
+
+
+
